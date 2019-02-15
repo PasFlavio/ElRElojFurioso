dc70bd1b0f26e4dc0d5b73d74a0527effbe36e61
ROMANIELLO CORENTIN <corentin.rmnll@eduge.ch>
2019-02-15 11:12:50 +0100

    Ajout horloge anglais

--- HorlogeAng.php
+++ HorlogeAng.php
@@ -1,8 +1,146 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+ <table border="1" cellspacing="5">
+            <thead>
+                <!-- I T L I S A S A M P M -->
+                <tr>
+                    <td class="il">I</td>
+                    <td class="il">T</td>
+                    <td>L</td>
+                    <td class="est">I</td>
+                    <td class="est">S</td>
+                    <td>A</td>
+                    <td>S</td>
+                    <td class="matin">A</td>
+                    <td class="matin">M</td>
+                    <td class="apresmidi">P</td>
+                    <td class="apresmidi">M</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <!-- Q U A T R E T R O I S -->
+                    <td class="un">A</td>
+                    <td>C</td>
+                    <td class="quart">Q</td>
+                    <td class="quart">U</td>
+                    <td class="quart">A</td>
+                    <td class="quart">R</td>
+                    <td class="quart">T</td>
+                    <td class="quart">E</td>
+                    <td class="quart">R</td>
+                    <td>D</td>
+                    <td>C</td>
+                </tr>
+                <tr>
+                    <!-- N E U F U N E S E P T -->
+                    <td class="vingt">T</td>
+                    <td class="vingt">W</td>
+                    <td class="vingt">E</td>
+                    <td class="vingt">N</td>
+                    <td class="vingt">T</td>
+                    <td class="vingt">Y</td>
+                    <td class="cinq">F</td>
+                    <td class="cinq">I</td>
+                    <td class="cinq">V</td>
+                    <td class="cinq">E</td>
+                    <td>X</td>
+                </tr>
+                <tr>
+                    <!-- H U I T S I X C I N Q -->
+                    <td class="demi">H</td>
+                    <td class="demi">A</td>
+                    <td class="demi">L</td>
+                    <td class="demi">F</td>
+                    <td>S</td>
+                    <td class="dix">T</td>
+                    <td class="dix">E</td>
+                    <td class="dix">N</td>
+                    <td>F</td>
+                    <td class="pour">T</td>
+                    <td class="pour">O</td>
+                </tr>
+                <tr>
+                    <!-- M I D I X M I N U I T -->
+                    <td class="apres">P</td>
+                    <td class="apres">A</td>
+                    <td class="apres">S</td>
+                    <td class="apres">T</td>
+                    <td>E</td>
+                    <td>R</td>
+                    <td>U</td>
+                    <td class="neuf">N</td>
+                    <td class="neuf">I</td>
+                    <td class="neuf">N</td>
+                    <td class="neuf">E</td>
+                </tr>
+                <tr>
+                    <!-- O N Z E R H E U R E S -->
+                    <td class="un">O</td>
+                    <td class="un">N</td>
+                    <td class="un">E</td>
+                    <td class="six">S</td>
+                    <td class="six">I</td>
+                    <td class="six">X</td>
+                    <td class="trois">T</td>
+                    <td class="trois">H</td>
+                    <td class="trois">R</td>
+                    <td class="trois">E</td>
+                    <td class="trois">E</td>
+                </tr>
+                <tr>
+                    <!-- M O I N S O L E D I X -->
+                    <td class="quatre">F</td>
+                    <td class="quatre">O</td>
+                    <td class="quatre">U</td>
+                    <td class="quatre">R</td>
+                    <td class="cinq">F</td>
+                    <td class="cinq">I</td>
+                    <td class="cinq">V</td>
+                    <td class="cinq">E</td>
+                    <td class="deux">T</td>
+                    <td class="deux">W</td>
+                    <td class="deux">O</td>
+                </tr>
+                <tr>
+                    <!-- E T R Q U A R T P M D -->
+                    <td class="huit">E</td>
+                    <td class=""huit>I</td>
+                    <td class="huit">G</td>
+                    <td class="huit">H</td>
+                    <td class="huit">T</td>
+                    <td class="onze">E</td>
+                    <td class="onze">L</td>
+                    <td class="onze">E</td>
+                    <td class="onze">V</td>
+                    <td class="onze">E</td>
+                    <td class="onze">N</td>
+                </tr>
+                <tr>
+                    <!-- V I N G T - C I N Q  -->
+                    <td class="sept">S</td>
+                    <td class="sept">E</td>
+                    <td class="sept">V</td>
+                    <td class="sept">E</td>
+                    <td class="sept">N</td>
+                    <td class="douze">T</td>
+                    <td class="douze">W</td>
+                    <td class="douze">E</td>
+                    <td class="douze">L</td>
+                    <td class="douze">V</td>
+                    <td class="douze">E</td>
+                </tr>
+                <tr>
+                    <!-- E T S D E M I E P A M -->
+                    <td class="dix">T</td>
+                    <td class="dix">E</td>
+                    <td class="dix">N</td>
+                    <td>S</td>
+                    <td>E</td>
+                    <td class="pile">O'</td>
+                    <td class="pile">C</td>
+                    <td class="pile">L</td>
+                    <td class="pile">O</td>
+                    <td class="pile">C</td>
+                    <td class="pile">K</td>
+                </tr>
+            </tbody>
+        </table>
