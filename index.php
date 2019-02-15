c6ce1e5e1b658643eebf79b63e073eca281e3f6c
MORRONEF_INFO <flavio.mrrn@eduge.ch>
2019-02-15 10:25:27 +0100

    Ajout début du script

--- index.php
+++ index.php
@@ -157,9 +157,20 @@ V1.0
                 </tr>
             </tbody>
         </table>
-
-        <?php
         
-        ?>
+        <div id="Test"></div>
+        <script>
+            
+            let timer = setInterval(timer, 10);
+            
+            function timer(){
+                let heureFrance = moment().tz('Europe/Paris').format('H:mm');
+                let heureChine = moment().tz('Asie/Chine').format('H:mm');
+                let heureEtatsUnis = moment().tz('Amerique/Etats-Unis').format('H:mm');
+                
+                Test.innerHTML = "<p>Heure en France: "+heureFrance+"</p><p>Heure en Chine: "+heureChine+"</p><p>Heure aux Etats-Unis: "+heureEtatsUnis+"</p>"
+            }
+            
+        </script>
     </body>
 </html>
