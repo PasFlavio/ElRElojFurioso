8c44fd7fe220d133421f73a008a27f3858cc1f34
Carvalho <daniel.crvlh@eduge.ch>
2019-02-15 11:32:58 +0100

    Ajout gerer musique

--- index.php
+++ index.php
@@ -10,23 +10,40 @@ V1.0
         <link href="./CSS/main.css" rel="stylesheet" type="text/css"/>
         <title>Horloge Furieuse</title>
     </head>
-    <body>
+    <body onload='GererMusique();'>
         <header>
             
         </header>
        
+        <p>
+            <audio id='lecteurMp3' controls="controls" autoplay="autoplay" >
+                <source src="musiques/musique1.mp3" type="audio/mp3"/>
+            </audio>
+        </p>
         
         <div id="Test"></div>
+        
         <script>
             
-            let timer = setInterval(timer, 10);
+            var tabMusique = array("<source src='musiques/musique1.mp3' type='audio/mp3'/>", "<source src='musiques/musique2.mp3' type='audio/mp3'/>", "<source src='musiques/musique3.mp3' type='audio/mp3'/>", "<source src='musiques/musique4.mp3' type='audio/mp3'/>")
+            
+            var timer = setInterval(timer, 10);
+            
+            var lecteurMp3 = document.getElementById('lecteurMp3');
             
             function timer(){
-                let heureFrance = moment().tz('Europe/Paris').format('H:mm');
-                let heureChine = moment().tz('Asie/Chine').format('H:mm');
-                let heureEtatsUnis = moment().tz('Amerique/Etats-Unis').format('H:mm');
+                var heureFrance = moment().tz('Europe/Paris').format('H:mm');
+                var heureChine = moment().tz('Asie/Chine').format('H:mm');
+                var heureEtatsUnis = moment().tz('Amerique/Etats-Unis').format('H:mm');
                 
                 Test.innerHTML = "<p>Heure en France: "+heureFrance+"</p><p>Heure en Chine: "+heureChine+"</p><p>Heure aux Etats-Unis: "+heureEtatsUnis+"</p>"
+            }
+            
+            
+            var tmrMusique = setInterval(GererMusique, 1);
+            
+            function GererMusique() {
+                lecteurMp3.innerHTML = tabMusique[1];
             }
             
         </script>
