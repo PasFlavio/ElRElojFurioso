34fd108aca787a45c59803430aff9591311cb74e
MORRONEF_INFO <flavio.mrrn@eduge.ch>
2019-02-15 09:15:28 +0100

    Début du projet

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+MORRONE Flavio, TAYAN Jad, CARVALHO Daniel, ROMANIELLO Corentin,(MARCHAL Elijah)
+Horloge tel les Qlock
+V1.0
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="CSS/main.css" rel="stylesheet" type="text/css"/>
+        <title></title>
+    </head>
+    <body>
+        <?php
+        
+        ?>
+    </body>
+</html>
