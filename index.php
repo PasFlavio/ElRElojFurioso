20cef1344d17a73f14118d17e70801a1bee86ef3
Carvalho <daniel.crvlh@eduge.ch>
2019-03-01 08:36:55 +0100

    add html musique

--- index.php
+++ index.php
@@ -12,40 +12,40 @@ V1.0
     </head>
     <body onload='GererMusique();'>
         <header>
-            
+
         </header>
-       
+
         <p>
             <audio id='lecteurMp3' controls="controls" autoplay="autoplay" >
                 <source src="musiques/musique1.mp3" type="audio/mp3"/>
             </audio>
         </p>
-        
+
         <div id="Test"></div>
-        
+
         <script>
-            
+
             var tabMusique = array("<source src='musiques/musique1.mp3' type='audio/mp3'/>", "<source src='musiques/musique2.mp3' type='audio/mp3'/>", "<source src='musiques/musique3.mp3' type='audio/mp3'/>", "<source src='musiques/musique4.mp3' type='audio/mp3'/>")
-            
+
             var timer = setInterval(timer, 10);
-            
+
             var lecteurMp3 = document.getElementById('lecteurMp3');
-            
-            function timer(){
+
+            function timer() {
                 var heureFrance = moment().tz('Europe/Paris').format('H:mm');
                 var heureChine = moment().tz('Asie/Chine').format('H:mm');
                 var heureEtatsUnis = moment().tz('Amerique/Etats-Unis').format('H:mm');
-                
-                Test.innerHTML = "<p>Heure en France: "+heureFrance+"</p><p>Heure en Chine: "+heureChine+"</p><p>Heure aux Etats-Unis: "+heureEtatsUnis+"</p>"
+
+                Test.innerHTML = "<p>Heure en France: " + heureFrance + "</p><p>Heure en Chine: " + heureChine + "</p><p>Heure aux Etats-Unis: " + heureEtatsUnis + "</p>"
             }
-            
-            
+
+
             var tmrMusique = setInterval(GererMusique, 1);
-            
+
             function GererMusique() {
                 lecteurMp3.innerHTML = tabMusique[1];
             }
-            
+
         </script>
     </body>
 </html>
