f2315921ffa5275309dbd04c8a096301cac9a385
ROMANIELLC_INFO <corentin.rmnll@eduge.ch>
2019-03-08 09:07:49 +0100

    Ajout de la page gérant la musique dans l'index.php

--- index.php
+++ index.php
@@ -5,45 +5,21 @@ Horloge tel les Qlock
 V1.0
 -->
 <html>
+
     <head>
+
         <meta charset="UTF-8">
         <link href="./CSS/main.css" rel="stylesheet" type="text/css"/>
         <title>Horloge Furieuse</title>
+
     </head>
-    <body onload='GererMusique();'>
-        <header>
 
-        </header>
+    <body>
 
-        <p>
-            <audio id='lecteurMp3' controls="controls" autoplay="autoplay" >
-                <source src="musiques/musique1.mp3" type="audio/mp3"/>
-            </audio>
-        </p>
+        <?php
+        include_once 'musique.html';
+        ?>
 
-        <div id="Test"></div>
-
-        <script>
-
-            var tabMusique = array("<source src='musiques/musique1.mp3' type='audio/mp3'/>", "<source src='musiques/musique2.mp3' type='audio/mp3'/>", "<source src='musiques/musique3.mp3' type='audio/mp3'/>", "<source src='musiques/musique4.mp3' type='audio/mp3'/>")
-
-            var timer = setInterval(timer, 10);
-
-            var lecteurMp3 = document.getElementById('lecteurMp3');
-
-            function timer() {
-                var date = new Date(Date.now());
-                var minuteLocal = date.getMinute;
-                var heureLocal = date.getHours();
-            }
-
-
-            var tmrMusique = setInterval(GererMusique, 1);
-
-            function GererMusique() {
-                lecteurMp3.innerHTML = tabMusique[1];
-            }
-
-        </script>
     </body>
+
 </html>
