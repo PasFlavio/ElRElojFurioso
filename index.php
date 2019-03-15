00d57ee108a7e1579671395b2784ad18d9384d03
ElijahMRCHL <elijah.mrchl@eduge.ch>
2019-03-15 09:23:19 +0100

    void

--- index.php
+++ index.php
@@ -1,25 +1,47 @@
 <!DOCTYPE html>
 <!--
-MORRONE Flavio, TAYAN Jad, CARVALHO Daniel, ROMANIELLO Corentin,(MARCHAL Elijah)
+MORRONE Flavio, TAYAN Jad, MARCHAL Elijah, CARVALHO Daniel, ROMANIELLO Corentin
 Horloge tel les Qlock
 V1.0
 -->
 <html>
-
     <head>
-
         <meta charset="UTF-8">
         <link href="./CSS/main.css" rel="stylesheet" type="text/css"/>
         <title>Horloge Furieuse</title>
-
     </head>
+    <body id='body'>
+        <header>
+            <button id="btnThemeViolet" value="violet" name="btnThemeGris" class="btn" onclick="Bouton(this)">Thème Violet</button>
+            <button id="btnThemeRose" value="rose" name="btnThemeRose" class="btn" onclick="Bouton(this)">Thème Rose</button>
+            <button id="btnThemeNoir" value="noir" name="btnThemeNoir" class="btn" onclick="Bouton(this)">Thème Noir</button>
+            <div id='conteneurMusique'>
+                <?php
+                include_once 'musique.html';
+                ?>
+            </div>
+            <script>
+                document.getElementById("body").style = "background-color: #682AA1";
+                // Script de gestion du menu
+                function Bouton(sender) {
+                    var theme = sender.value;
 
-    <body>
-
-        <?php
-        include_once 'musique.html';
-        ?>
-
+                    if (theme === "violet") {
+                        document.getElementById("body").style = "background-color: #682AA1";
+                    }
+                    if (theme === "rose") {
+                        document.getElementById("body").style = "background-color: #E044A9";
+                    }
+                    if (theme === "noir") {
+                        document.getElementById("body").style = "background-color: #1d1d1d";
+                    }
+                }
+            </script>
+        </header>
+        <div>
+            <?php
+            include_once 'HorlogeFR.php';
+            ?>
+        </div>
     </body>
-
 </html>
