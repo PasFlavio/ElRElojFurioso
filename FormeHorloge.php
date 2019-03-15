8adf5330701bff4aa8042b39b5aeafba67b84eec
ROMANIELLC_INFO <corentin.rmnll@eduge.ch>
2019-03-15 09:39:23 +0100

    Ajout Horloge Ang Finis

--- FormeHorloge.php
+++ FormeHorloge.php
@@ -3,35 +3,32 @@
 <div id="Carre">
 
     <?php
-
-
-  include 'HorlogeFR.php';
-
-  ?>
+    include 'HorlogeFR.php';
+    ?>
     <a id="Triangle" value="Triangle" onclick="changementForme(this)">Triangle</a>
     <a id="Carre" value="Carre" onclick="changementForme(this)">Carre</a>
     <a id="Rond" value="Rond" onclick="changementForme(this)">Rond</a>
-    
+
     <script>
         FormeActu = "";
         FormeActu = "Carre";
-        
-    function changementForme(obj){
-        console.log(obj.innerHTML);
-        console.log(FormeActu);
-        
-        if (obj.innerHTML == "Triangle") {
-           document.getElementById(FormeActu).id = "Triangle";
-           FormeActu = "Triangle";
+
+        function changementForme(obj) {
+            console.log(obj.innerHTML);
+            console.log(FormeActu);
+
+            if (obj.innerHTML == "Triangle") {
+                document.getElementById(FormeActu).id = "Triangle";
+                FormeActu = "Triangle";
+            }
+            if (obj.innerHTML == "Carre") {
+                document.getElementById(FormeActu).id = "Carre";
+                FormeActu = "Carre";
+            }
+            if (obj.innerHTML == "Rond") {
+                document.getElementById(FormeActu).id = "Rond";
+                FormeActu = "Rond";
+            }
         }
-        if (obj.innerHTML == "Carre") {
-           document.getElementById(FormeActu).id = "Carre";
-           FormeActu = "Carre";
-        }
-        if (obj.innerHTML == "Rond") {
-           document.getElementById(FormeActu).id = "Rond";
-           FormeActu = "Rond";
-        }
-    }
     </script>
 </div>
